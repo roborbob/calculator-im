8ce1e052da32a1219bbc8eeec57a0089287079f7
Robert James <36674828+roborbob@users.noreply.github.com>
2020-03-16 02:06:29 +0000

    csv create

--- index.php
+++ index.php
@@ -17,6 +17,48 @@
         exit(0);
     }
 
+
+    // Get the IP address, cant seem to reach the HTTP_CLIENT_IP this way, needs further work.
+    function getUserIpAddr(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //ip pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+    
+    $ip_address = getUserIpAddr();
+
+    // Get POST request details
     $json = file_get_contents("php://input");
-    var_dump($json)
+
+    // Converts it into a PHP object
+    $data = json_decode($json);  
+    
+    // Store variables
+    $result = $data->result;
+    $time = $data->time;
+
+    $dataArray = array($result, $ip_address, $time);
+    
+    $filename = './data.csv';
+   
+    // Check if we are appending new data or creating new a whole new file
+    if(file_exists($filename)){
+        $fs = fopen("data.csv","a");
+        fputcsv($fs, $dataArray);
+        fclose($fs);
+        
+    } else {
+        $headers = array("Results","IP","Date");
+        $fh = fopen("data.csv", "w");
+        fputcsv($fh, $headers);
+        fputcsv($fh, $dataArray);
+        fclose($fh);
+    }   
 ?>
