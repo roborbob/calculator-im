16c7d69f609811f053203c41720e66ec31e765ee
Robert James <36674828+roborbob@users.noreply.github.com>
2020-03-16 02:34:00 +0000

    table styling

--- calculations.php
+++ calculations.php
@@ -3,6 +3,30 @@
         <title>Calculator</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style>
+            h1 {
+                text-align: center;
+                font-weight: 300;
+                margin: 20px;
+                font-familY: "Helvetica";
+            }
+            table {
+                margin: 0 auto;
+                border: 1px solid black;
+                width: fit-content;
+            }
+            tbody {
+                background: lightblue;
+            }
+            td {
+                font-weight: 300;
+                font-family: "Helvetica";
+                font-size: 15px;
+                padding: 10px;
+                width: 150px;
+            }
+        </style>
+    </head>
     <body> 
         <h1>Calculator Log</h1>   
         <div class="container">
