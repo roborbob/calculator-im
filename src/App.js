e2c9720bf0795cf23775243a14c06fe7c97fcd31
Robert James <36674828+roborbob@users.noreply.github.com>
2020-03-16 00:50:38 +0000

    clear+save buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import Button from './components/Button/button';
 
+
 function App() {
+  const [output, setOutput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const clearHandler = () => {
+
+  }
+
+  const saveHandler = () => {
+
+  }
+
   return (
     <div className="calculator">
       <section className="screen">
-    
       </section>
       <section className="keypad">
         <div className="column-one">
-          <div className="clear-save">          
+          <div className="clear-save">
+            <Button
+              value="AC"
+              clicked={clearHandler}/>
+            <Button
+              value="SAVE"
+              style={{width: "160px"}}
+              active={result ? false : true}
+              clicked={saveHandler}
+              />           
           </div>
           <div className="numbers">
           </div>
